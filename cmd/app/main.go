82fd34319c864dcf565670e682b01cbca46e829d
Karangin Stepan <s.karangin@dbfhub.com>
2025-04-05 15:42:37 +0300

    feat: add empty config and app init

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/xamust/ekom-test/config"
+	"github.com/xamust/ekom-test/internal/application"
+)
 
+func main() {
+	ctx := context.Background()
+	cfg, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+	application.RunWithCtx(ctx, cfg)
 }
---
+++ internal/application/app.go
@@ -0,0 +1,10 @@
+package application
+
+import (
+	"context"
+	"github.com/xamust/ekom-test/config"
+)
+
+func RunWithCtx(ctx context.Context, cfg *config.Config) {
+
+}
